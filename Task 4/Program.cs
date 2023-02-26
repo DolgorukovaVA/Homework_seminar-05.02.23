956ed9a72e8c8c66e256b12e0e19b9316420b5d0
Valentina <west.77@mail.ru>
2023-02-26 03:23:26 +0300

    Task 4 added

---
+++ Task 4/Program.cs
@@ -0,0 +1,36 @@
+﻿// Сформировать трехмерный массив не повторяющимися двузначными числами,
+// показать его построчно на экран выводя индексы соответствующего элемента
+
+Console.WriteLine("Введите число a;");
+int a = int.Parse(Console.ReadLine() ?? "");
+Console.WriteLine("Введите число b;");
+int b = int.Parse(Console.ReadLine() ?? "");
+Console.WriteLine("Введите число c;");
+int c = int.Parse(Console.ReadLine() ?? "");
+Random rnd = new Random();
+int[,,] matrix = new int[a, b, c];
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        for (int l = 0; l < matrix.GetLength(2); l++)
+        {
+            matrix[i, j, l] = rnd.Next(100);
+        }
+    }
+}
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+
+        for (int l = 0; l < matrix.GetLength(2); l++)
+        {
+            Console.Write($"{matrix[i, j, l],1}({i},{j},{l}) ");
+        }
+        Console.WriteLine();
+    }
+}
+
